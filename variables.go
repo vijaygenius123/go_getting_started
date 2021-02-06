7cadcd09b59e78a7e66f9deea2ff36af74ad8155
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-06 20:54:57 +0530

    Basic Variables

--- variables.go
+++ variables.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hello World")
+func main() {
+	fmt.Println("Variables")
+
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Vijay"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(1, 2)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
